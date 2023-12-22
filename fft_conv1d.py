960c337e80df17e89568231f997a0a3593462b2c
Brendan Gavin <brendanedwardgavin@gmail.com>
2023-12-22 10:40:42 -0800

    add 1d convolutions, tests, requirements.txt

---
+++ fft_conv1d.py
@@ -0,0 +1,35 @@
+from math import  floor
+
+import torch
+import torch.nn.functional as F
+
+def fft_conv1d(x, kernel):
+    """
+    Simple 1D convolution using FFT that returns same value as torch.functional.conv1d(x, kernel, padding="same"). Assumes 1 channel.
+    """
+    # channels = 1
+    assert kernel.shape[1] == 1 and kernel.shape[0] == 1 
+    
+    k = kernel.shape[-1]
+    kernel_padded = F.pad(kernel.flip(2), (0, x.shape[-1]-k), mode="constant")
+    kernel_padded = kernel_padded.roll(-floor(k/2), dims=2)
+    kernel_fft = torch.fft.fft(kernel_padded)
+    x_fft = torch.fft.fft(x)
+    y_fft = torch.fft.ifft(kernel_fft * x_fft).real
+    return y_fft
+
+def fft_inv_conv1d(x, kernel):
+    """
+    Simple inverse of 1D convolution using FFT. Assumes 1 channel.
+    """
+    # channels = 1
+    assert kernel.shape[1] == 1 and kernel.shape[0] == 1 
+    
+    k = kernel.shape[-1]
+    kernel_padded = F.pad(kernel.flip(2), (0, x.shape[-1]-k), mode="constant")
+    kernel_padded = kernel_padded.roll(-floor(k/2), dims=2)
+    kernel_fft = torch.fft.fft(kernel_padded)
+    x_fft = torch.fft.fft(x)
+    #this is the only line that is different from fft_conv1d:
+    y_fft = torch.fft.ifft(x_fft/kernel_fft).real 
+    return y_fft
---
+++ test_fft_conv1d.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn.functional as F
+import pytest
+
+from fft_conv1d import fft_conv1d, fft_inv_conv1d
+
+TEST_TOL = 1e-5
+
+@pytest.mark.parametrize("signal_dim", [31, 32, 34])
+@pytest.mark.parametrize("kernel_dim", [2, 3, 4, 5, 6])
+def test_fft_conv1d(signal_dim, kernel_dim):
+    B, C = 1, 1
+    N = signal_dim
+    K = kernel_dim
+
+    kernel = torch.randn(C, 1, K)
+    x = torch.randn(B, C, N)
+    
+    y_torch = F.conv1d(
+        F.pad(x, (K//2+1, K//2+1), mode="circular"), 
+        kernel, padding="same")[:,:,K//2+1:-(K//2+1)]
+    y_fft = fft_conv1d(x, kernel)
+    print(x.shape, y_torch.shape, y_fft.shape)
+    
+    assert torch.norm(y_torch-y_fft)/torch.norm(y_torch) < TEST_TOL
+    
+@pytest.mark.parametrize("signal_dim", [31, 32, 34])
+@pytest.mark.parametrize("kernel_dim", [1, 2, 3, 4, 5, 6])
+def test_fft_inv_conv1d(signal_dim, kernel_dim):
+    B, C = 1, 1
+    N = signal_dim
+    K = kernel_dim
+
+    device = torch.device("cuda:0")
+    kernel = torch.randn(C, 1, K).to(device)
+    x = torch.randn(B, C, N).to(device)
+    
+    y_torch = F.conv1d(
+        F.pad(x, (K//2+1, K//2+1), mode="circular"),
+        kernel, padding="same")[:,:,K//2+1:-(K//2+1)]
+    y_fft = fft_conv1d(x, kernel)
+    
+    x_torch_inv = fft_inv_conv1d(y_torch, kernel)
+    x_fft_inv = fft_inv_conv1d(y_fft, kernel)
+    
+    x_fft_res = torch.norm(x_fft_inv-x)/torch.norm(x)
+    x_torch_res = torch.norm(x_torch_inv-x)/torch.norm(x)
+    
+    assert x_fft_res < TEST_TOL
+    assert x_torch_res < TEST_TOL
