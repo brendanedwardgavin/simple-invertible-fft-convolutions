a287a4b1d4cb5371582627b02403292308d0a917
Brendan Gavin <brendanedwardgavin@gmail.com>
2023-12-23 12:37:10 -0800

    encapsulate kernel adjustments

--- fft_conv2d.py
+++ fft_conv2d.py
@@ -3,16 +3,9 @@ from math import  floor
 import torch
 import torch.nn.functional as F
 
-def fft_conv2d(x, kernel):
-    """
-    Simple 2D convolution using FFT that returns same value as 
-    torch.nn.Conv2d(1, 1, kernel_size, padding="same", bias=False, padding_mode='circular')
-    """
-    # channels = 1
-    assert kernel.shape[1] == 1 and kernel.shape[0] == 1 
-
+def adjust_kernel_to_torch(kernel, x_shape):
     k_shape = kernel.shape[2:]
-    x_shape = x.shape[2:]
+    x_shape = x_shape[2:]
     
     k_padding = (
         0, x_shape[-1]-k_shape[-1],
@@ -27,6 +20,38 @@ def fft_conv2d(x, kernel):
     kernel_padded = F.pad(kernel_padded, k_padding, mode="constant")
     kernel_padded = kernel_padded.roll(k_rolling, dims=dims)
     
+    return kernel_padded
+
+def undo_adjust_kernel_to_torch(kernel, x_shape):
+    k_shape = kernel.shape[2:]
+    x_shape = x_shape[2:]
+    
+    k_padding = (
+        0, x_shape[-1]-k_shape[-1],
+        0, x_shape[-2]-k_shape[-2],
+    )
+    k_rolling = (
+        -floor(k_shape[0]/2),
+        -floor(k_shape[1]/2),
+    )
+    dims = (2, 3)
+    kernel_padded = kernel.flip(dims)
+    kernel_padded = F.pad(kernel_padded, k_padding, mode="constant")
+    kernel_padded = kernel_padded.roll(k_rolling, dims=dims)
+    
+    return kernel_padded
+
+def fft_conv2d(x, kernel):
+    """
+    Simple 2D convolution using FFT that returns same value as 
+    torch.nn.Conv2d(1, 1, kernel_size, padding="same", bias=False, padding_mode='circular')
+    """
+    # channels = 1
+    assert kernel.shape[1] == 1 and kernel.shape[0] == 1 
+
+    kernel_padded = adjust_kernel_to_torch(kernel, x.shape)
+    
+    dims = (2, 3)
     kernel_fft = torch.fft.fftn(kernel_padded, dim=dims)
     x_fft = torch.fft.fftn(x, dim=dims)
     y_fft = torch.fft.ifftn(kernel_fft * x_fft, dim=dims).real
@@ -40,21 +65,9 @@ def fft_inv_conv2d(x, kernel):
     # channels = 1
     assert kernel.shape[1] == 1 and kernel.shape[0] == 1 
 
-    k_shape = kernel.shape[2:]
-    x_shape = x.shape[2:]
+    kernel_padded = adjust_kernel_to_torch(kernel, x.shape)
     
-    k_padding = (
-        0, x_shape[-1]-k_shape[-1],
-        0, x_shape[-2]-k_shape[-2],
-    )
-    k_rolling = (
-        -floor(k_shape[0]/2),
-        -floor(k_shape[1]/2),
-    )
     dims = (2, 3)
-    kernel_padded = kernel.flip(dims)
-    kernel_padded = F.pad(kernel_padded, k_padding, mode="constant")
-    kernel_padded = kernel_padded.roll(k_rolling, dims=dims)
     
     kernel_fft = torch.fft.fftn(kernel_padded, dim=dims)
     x_fft = torch.fft.fftn(x, dim=dims)
